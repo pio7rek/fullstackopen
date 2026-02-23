c64002491f5aa13f65e5a3b33ae0d1c5e4296737
Piotr Połomski <pi7rek@gmail.com>
2026-02-23 20:09:45 +0100

    Exercise 1.11*: unicafe step 6

--- part_1/unicafe/src/App.jsx
+++ part_1/unicafe/src/App.jsx
@@ -2,7 +2,11 @@ import { useState } from 'react'
 
 const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button> 
 
-const StatisticLine = ({text, value}) => <div>{text} {value}</div>
+const StatisticLine = ({text, value}) => 
+<tr>
+  <td>{text}</td>
+  <td>{value}</td>
+</tr>
 
 const Statistics = ({good, neutral, bad}) => {
   const all = good + neutral + bad
@@ -22,12 +26,16 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <>
       <h2>statistics</h2>
-      <StatisticLine text='good' value={good} />
-      <StatisticLine text='neutral' value={neutral} />
-      <StatisticLine text='bad' value={bad} />
-      <StatisticLine text='all' value={all} />
-      <StatisticLine text='average' value={average} />
-      <StatisticLine text='positive' value={positive + ' %'} />
+      <table>
+        <tbody>
+          <StatisticLine text='good' value={good} />
+          <StatisticLine text='neutral' value={neutral} />
+          <StatisticLine text='bad' value={bad} />
+          <StatisticLine text='all' value={all} />
+          <StatisticLine text='average' value={average} />
+          <StatisticLine text='positive' value={positive + ' %'} />
+        </tbody>
+      </table>
     </>
   )
 }
